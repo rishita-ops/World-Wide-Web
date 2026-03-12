c28053f889e9207293e2c5633e6561dd1b025f7f
Rishita Sanghavi <rishita.sanghavi07@gmail.com>
2026-03-12 19:06:36 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,33 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() 
+{
     string s;
+    cout<<"Enter the URL : ";
     cin >> s;
 
     string prefix;
     int start;
 
-    if (s.substr(0, 7) == "http://") {
+    if (s.substr(0, 7) == "http://") 
+    {
         prefix = "http://";
         start = 7;
-    } else if (s.substr(0, 8) == "https://") {
+    } 
+    else if (s.substr(0, 8) == "https://") 
+    {
         prefix = "https://";
         start = 8;
-    } else {
+    } 
+    else 
+    {
         cout << "invalid";
         return 0;
     }
 
     string rest = s.substr(start); 
 
-    if (rest.find('.') == string::npos || rest.empty()) {
+    if (rest.find('.') == string::npos || rest.empty()) 
+    {
         cout << "invalid";
         return 0;
     }
 
     bool valid = true;
-    for (char c : rest) {
+    for (char c : rest) 
+    {
         if (!(isalnum(c) || c == '-' || c == '.' || c == '/' ||
               c == '_' || c == '?' || c == '=' || c == '&' ||
               c == '#' || c == '%' || c == '+' || c == ':' || c == '@')) {
